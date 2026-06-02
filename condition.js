8e6c8f599c402d35c69d989b53006781e8227a36
Divyanshi Srivastava <241068726+divyanshisrivastava395@users.noreply.github.com>
2026-06-02 10:45:05 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,9 @@
+let fruits=["apple","banana","cherry"];
+console.log(fruits[0]);
+console.log(fruits[1])
+console.log(fruits[2])
+console.log(fruits);
+fruits.push("date");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
